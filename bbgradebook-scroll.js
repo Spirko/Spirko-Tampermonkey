1fe1a948eade04b0d657ab9d0e3d4581aa4747ed
Spirko <spirko@gmail.com>
2021-12-11 19:34:46 -0600

    Update bbgradebook-scroll.js
    Now keeps working after grade table is recreated (due to window resize, etc.).

--- bbgradebook-scroll.js
+++ bbgradebook-scroll.js
@@ -1,31 +1,46 @@
 // ==UserScript==
 // @name         Blackboard Gradebook Scroller
 // @namespace    http://luspirko.com/
-// @version      0.2
+// @version      0.3
 // @description  Improved scrollwheel handling for Blackboard Gradebook.
 // @author       Jeff Spirko
 // @match        https://*/webapps/gradebook/do/instructor/enterGradeCenter*
 // @homepage     https://github.com/Spirko/Spirko-Tampermonkey
+// @require      https://cdn.jsdelivr.net/npm/lil-gui@0.15
 // @grant        none
-// @run-at       context-menu
 // ==/UserScript==
 
-(function() {
+;(function() {
   'use strict';
-  console.log("ImpatientProf's Blackboard Gradebook Scroller");
+  let name = "Spirko's Blackboard Gradebook Scroller";
+  console.log(name + " startup");
 
-  // The gradebook table is actually a non-scrollable table with fake scrollbars.
-  let t = document.querySelector('.gbtable');
-  // Two elements up is the container, but grab it by its id, probably table1_container.
-  let tc = document.querySelector('#' + t.id + '_container');
-  // The container has up to 3 children: viewport, vertical scrollbar, horizontal scrollbar.
-  let tv = tc.children[1];
-  let th = tc.children[2];
-  // Figure out if the existing scrollbar is vertical or horizontal.
-  if (!th && tv.style.overflowX == 'scroll') {
-    th = tv;
-    tv = undefined;
+  let tc, tv, th;
+  let status = { }
+  status.running = true;
+
+  const gui = new lil.GUI({title: 'Blackboard Scoller'});
+  gui.add(status, "running").onChange( value => { if (value) setTimeout(doSetup, 500); });
+
+  function doSetup() {
+    // console.log(name + " setup");
+    // The gradebook table is actually a non-scrollable table with fake scrollbars.
+    let tid = document.querySelector('.gbtable').id;
+    // Two elements up is the container, but grab it by its id, probably table1_container.
+    tc = Array.from(document.querySelector('#' + tid + '_container').children);
+    // The container has up to 3 children: viewport, vertical scrollbar, horizontal scrollbar.
+    tv = tc[1];
+    th = tc[2];
+    // Figure out if the existing scrollbar is vertical or horizontal.
+    if (!th && tv.style.overflowX == 'scroll') {
+      th = tv;
+      tv = undefined;
+    }
+    tc[0].addEventListener('wheel', doScroll, {passive: false});
+
+    if (status.running) setTimeout(doSetup, 1000);
   }
+  if (status.running) setTimeout(doSetup, 1000);
 
   function doScroll(event) {
     if (event.ctrlKey) return; // don't interfere with ctrl-wheel for zooming.
@@ -40,6 +55,5 @@
     }
 
   }
-  t.addEventListener('wheel', doScroll);
 
 })();
