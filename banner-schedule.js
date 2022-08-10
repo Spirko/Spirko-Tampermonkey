854babf525bb411e12336c00be8bb5617126a01d
Spirko <spirko@gmail.com>
2022-08-10 13:26:26 -0500

    Template banner-schedule.js

---
+++ banner-schedule.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Blackboard Gradebook Scroller
+// @namespace    http://luspirko.com/
+// @version      0.3
+// @description  Visual and Functional tweaks to TAMUCC public schedule.
+// @author       Jeff Spirko
+// @match        https://banner.tamucc.edu/schedule/BPROD.php
+// @homepage     https://github.com/Spirko/Spirko-Tampermonkey
+// @require      https://cdn.jsdelivr.net/npm/lil-gui@0.15
+// @grant        none
+// ==/UserScript==
+
+;(function() {
+  'use strict';
+  
+  console.log(`Loaded Spirko's banner-schedule.js`);
+})()
